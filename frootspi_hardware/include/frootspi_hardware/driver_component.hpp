a93d262f98d4928e54e1fd9f1b8f2856e70cd1fb
ShotaAk <macakasit@gmail.com>
2021-06-16 14:16:03 +0000

    Update

--- frootspi_hardware/include/frootspi_hardware/driver_component.hpp
+++ frootspi_hardware/include/frootspi_hardware/driver_component.hpp
@@ -20,12 +20,19 @@
 #include <vector>
 
 #include "frootspi_hardware/visibility_control.h"
+#include "frootspi_msgs/msg/ball_detection.hpp"
+#include "frootspi_msgs/msg/battery_voltage.hpp"
+#include "frootspi_msgs/msg/dribble_power.hpp"
+#include "frootspi_msgs/msg/switches_state.hpp"
+#include "frootspi_msgs/msg/wheel_velocities.hpp"
+#include "frootspi_msgs/srv/kick.hpp"
+#include "frootspi_msgs/srv/set_kicker_charging.hpp"
+#include "frootspi_msgs/srv/set_lcd_text.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
 #include "rclcpp_lifecycle/lifecycle_publisher.hpp"
-#include "std_msgs/msg/int16.hpp"
-#include "std_msgs/msg/float32.hpp"
-#include "frootspi_msgs/msg/ball_detection.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "std_srvs/srv/set_bool.hpp"
 
 namespace frootspi_hardware
 {
@@ -36,10 +43,13 @@ public:
   FROOTSPI_HARDWARE_PUBLIC
   explicit Driver(const rclcpp::NodeOptions & options);
 
-protected:
-  void on_polling_timer();
-
 private:
+  void on_polling_timer();
+  void callback_dribble_power(const frootspi_msgs::msg::DribblePower::SharedPtr msg);
+  void callback_wheel_velocities(const frootspi_msgs::msg::WheelVelocities::SharedPtr msg);
+  void on_kick(const frootspi_msgs::srv::Kick::Request::SharedPtr request,
+               frootspi_msgs::srv::Kick::Response::SharedPtr response);
+
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
   on_configure(const rclcpp_lifecycle::State &);
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
@@ -52,6 +62,22 @@ private:
   on_shutdown(const rclcpp_lifecycle::State &);
 
   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::BallDetection>> pub_ball_detection_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::BatteryVoltage>> pub_battery_voltage_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::BatteryVoltage>> pub_ups_voltage_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::BatteryVoltage>> pub_kicker_voltage_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::SwitchesState>> pub_switches_state_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<frootspi_msgs::msg::WheelVelocities>> pub_present_wheel_velocities_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::Imu>> pub_imu_;
+
+  std::shared_ptr<rclcpp::Subscription<frootspi_msgs::msg::DribblePower>> sub_dribble_power_;
+  std::shared_ptr<rclcpp::Subscription<frootspi_msgs::msg::WheelVelocities>> sub_target_wheel_velocities_;
+
+  std::shared_ptr<rclcpp::Service<frootspi_msgs::srv::Kick>> srv_kick_;
+  std::shared_ptr<rclcpp::Service<frootspi_msgs::srv::SetKickerCharging>> srv_set_kicker_charging_;
+  std::shared_ptr<rclcpp::Service<frootspi_msgs::srv::SetLCDText>> srv_set_lcd_text_;
+  std::shared_ptr<rclcpp::Service<std_srvs::srv::SetBool>> srv_set_left_led_;
+  std::shared_ptr<rclcpp::Service<std_srvs::srv::SetBool>> srv_set_center_led_;
+  std::shared_ptr<rclcpp::Service<std_srvs::srv::SetBool>> srv_set_right_led_;
 
   rclcpp::TimerBase::SharedPtr polling_timer_;
 
--- frootspi_hardware/src/driver_component.cpp
+++ frootspi_hardware/src/driver_component.cpp
@@ -49,6 +49,29 @@ void Driver::on_polling_timer()
   pub_ball_detection_->publish(std::move(ball_detection_msg));
 }
 
+void Driver::callback_dribble_power(const frootspi_msgs::msg::DribblePower::SharedPtr msg)
+{
+  std::cout << "ドリブルパワーは" << std::to_string(msg->power) << std::endl;
+}
+
+void Driver::callback_wheel_velocities(const frootspi_msgs::msg::WheelVelocities::SharedPtr msg)
+{
+  std::cout << "車輪目標回転速度は、左前" << std::to_string(msg->front_left);
+  std::cout << "、真ん中後" << std::to_string(msg->back_center);
+  std::cout << "、右前" << std::to_string(msg->back_center) << std::endl;;
+}
+
+void Driver::on_kick(const frootspi_msgs::srv::Kick::Request::SharedPtr request,
+               frootspi_msgs::srv::Kick::Response::SharedPtr response)
+{
+  std::cout << "キックの種類:" << std::to_string(request->kick_type);
+  std::cout << ", キックパワー:" << std::to_string(request->kick_power) << std::endl;
+
+  response->success = true;
+  response->message = "キック成功したで";
+}
+
+
 CallbackReturn Driver::on_configure(const rclcpp_lifecycle::State &)
 {
   using namespace std::placeholders;  // for _1, _2, _3...
@@ -60,6 +83,19 @@ CallbackReturn Driver::on_configure(const rclcpp_lifecycle::State &)
   polling_timer_->cancel();
 
   pub_ball_detection_ = create_publisher<frootspi_msgs::msg::BallDetection>("ball_detection", 1);
+  pub_battery_voltage_ = create_publisher<frootspi_msgs::msg::BatteryVoltage>("battery_voltage", 1);
+  pub_ups_voltage_ = create_publisher<frootspi_msgs::msg::BatteryVoltage>("ups_voltage", 1);
+  pub_kicker_voltage_ = create_publisher<frootspi_msgs::msg::BatteryVoltage>("kicker_voltage", 1);
+  pub_switches_state_ = create_publisher<frootspi_msgs::msg::SwitchesState>("switches_state", 1);
+  pub_present_wheel_velocities_ = create_publisher<frootspi_msgs::msg::WheelVelocities>("present_wheel_velocities", 1);
+  pub_imu_ = create_publisher<sensor_msgs::msg::Imu>("imu", 1);
+
+  sub_dribble_power_ = create_subscription<frootspi_msgs::msg::DribblePower>(
+    "dribble_power", 1, std::bind(&Driver::callback_dribble_power, this, _1));
+  sub_target_wheel_velocities_ = create_subscription<frootspi_msgs::msg::WheelVelocities>(
+    "target_wheel_velocities", 1, std::bind(&Driver::callback_wheel_velocities, this, _1));
+
+  srv_kick_ = create_service<frootspi_msgs::srv::Kick>("kick", std::bind(&Driver::on_kick, this, _1, _2));
 
   pi_ = pigpio_start(NULL, NULL);
 
@@ -76,6 +112,13 @@ CallbackReturn Driver::on_activate(const rclcpp_lifecycle::State &)
   RCLCPP_INFO(this->get_logger(), "on_activate() is called.");
 
   pub_ball_detection_->on_activate();
+  pub_battery_voltage_->on_activate();
+  pub_ups_voltage_->on_activate();
+  pub_kicker_voltage_->on_activate();
+  pub_switches_state_->on_activate();
+  pub_present_wheel_velocities_->on_activate();
+  pub_imu_->on_activate();
+
   polling_timer_->reset();
 
   return CallbackReturn::SUCCESS;
@@ -86,6 +129,12 @@ CallbackReturn Driver::on_deactivate(const rclcpp_lifecycle::State &)
   RCLCPP_INFO(this->get_logger(), "on_deactivate() is called.");
 
   pub_ball_detection_->on_deactivate();
+  pub_battery_voltage_->on_deactivate();
+  pub_ups_voltage_->on_deactivate();
+  pub_kicker_voltage_->on_deactivate();
+  pub_switches_state_->on_deactivate();
+  pub_present_wheel_velocities_->on_deactivate();
+  pub_imu_->on_deactivate();
   polling_timer_->cancel();
 
   return CallbackReturn::SUCCESS;
@@ -96,6 +145,12 @@ CallbackReturn Driver::on_cleanup(const rclcpp_lifecycle::State &)
   RCLCPP_INFO(this->get_logger(), "on_cleanup() is called.");
 
   pub_ball_detection_.reset();
+  pub_battery_voltage_.reset();
+  pub_ups_voltage_.reset();
+  pub_kicker_voltage_.reset();
+  pub_switches_state_.reset();
+  pub_present_wheel_velocities_.reset();
+  pub_imu_.reset();
   polling_timer_.reset();
 
   pigpio_stop(pi_);
@@ -108,6 +163,12 @@ CallbackReturn Driver::on_shutdown(const rclcpp_lifecycle::State &)
   RCLCPP_INFO(this->get_logger(), "on_shutdown() is called.");
 
   pub_ball_detection_.reset();
+  pub_battery_voltage_.reset();
+  pub_ups_voltage_.reset();
+  pub_kicker_voltage_.reset();
+  pub_switches_state_.reset();
+  pub_present_wheel_velocities_.reset();
+  pub_imu_.reset();
   polling_timer_.reset();
 
   return CallbackReturn::SUCCESS;
