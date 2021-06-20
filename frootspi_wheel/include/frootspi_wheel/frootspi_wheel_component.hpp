70b85111b5ad0a0dfe04a744ee26dd63381af19d
tilt.silvie <tilt.silvie@gmail.com>
2021-06-20 15:12:26 +0000

    implement simple code

---
+++ frootspi_wheel/include/frootspi_wheel/frootspi_wheel_component.hpp
@@ -0,0 +1,48 @@
+// Copyright 2021 Roots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FROOTSPI_WHEEL__FROOTSPI_WHEEL_COMPONENT_HPP_
+#define FROOTSPI_WHEEL__FROOTSPI_WHEEL_COMPONENT_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
+#include "frootspi_wheel/visibility_control.h"
+
+namespace frootspi_wheel
+{
+
+class WheelNode : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+    FROOTSPI_WHEEL_PUBLIC
+    explicit WheelNode(const rclcpp::NodeOptions & options);
+
+private:
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_configure(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_activate(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_deactivate(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_cleanup(const rclcpp_lifecycle::State &);
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+  on_shutdown(const rclcpp_lifecycle::State &);
+};
+
+}  // namespace frootspi_wheel
+
+#endif  // FROOTSPI_WHEEL__FROOTSPI_WHEEL_COMPONENT_HPP_
---
+++ frootspi_wheel/src/frootspi_wheel_component.cpp
@@ -0,0 +1,71 @@
+// Copyright 2021 Roots
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "frootspi_wheel/frootspi_wheel_component.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int16.hpp"
+
+using namespace std::chrono_literals;
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+namespace frootspi_wheel
+{
+
+WheelNode::WheelNode(const rclcpp::NodeOptions & options)
+: rclcpp_lifecycle::LifecycleNode("wheel", options)
+{
+}
+
+
+CallbackReturn WheelNode::on_configure(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn WheelNode::on_activate(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn WheelNode::on_deactivate(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn WheelNode::on_cleanup(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+CallbackReturn WheelNode::on_shutdown(const rclcpp_lifecycle::State &)
+{
+  return CallbackReturn::SUCCESS;
+}
+
+}  // namespace frootspi_wheel
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+RCLCPP_COMPONENTS_REGISTER_NODE(frootspi_wheel::WheelNode)
