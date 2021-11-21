acee2b33d286b9befeebb1343f7d7c154cba240b
tilt-silvie <tilt.silvie@gmail.com>
2021-11-21 06:21:36 +0000

    ROS_DOMAIN_IDの設定を追加

--- frootspi_examples/systemd/robot_launch.sh
+++ frootspi_examples/systemd/robot_launch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ENVFILE=/home/ubuntu/ros2_ws/install/setup.bash
+ROS_DOMAIN_ID_VALUE=22
 
 if [ -f ${ENVFILE} ]; then
     #環境変数読み込み
     echo "Loading ROS2 Env..."
     source ${ENVFILE}
+    export ROS_DOMAIN_ID=${ROS_DOMAIN_ID_VALUE}
 
     echo "ROS2 Launching..."
     #roslaunch実行
