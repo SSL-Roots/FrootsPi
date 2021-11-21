e445addda8678f8771118633b221d10791573ac0
tilt-silvie <tilt.silvie@gmail.com>
2021-11-21 06:56:31 +0000

    IPアドレスからロボットのIDを取得する処理を追加

--- frootspi_examples/systemd/robot_launch.sh
+++ frootspi_examples/systemd/robot_launch.sh
@@ -9,9 +9,15 @@ if [ -f ${ENVFILE} ]; then
     source ${ENVFILE}
     export ROS_DOMAIN_ID=${ROS_DOMAIN_ID_VALUE}
 
+    # IPアドレスからロボットIDを取得
+    # IPアドレスの下2ケタがIDと対応している
+    IFACE="wlan0"
+    IP_ADDRESS=$(/sbin/ip -f inet -o addr show "${IFACE}" | cut -d\  -f 7 | cut -d/ -f 1)
+    ROBOT_ID=`echo ${IP_ADDRESS: -2:2} | sed -e 's/^0//g'`
+
     echo "ROS2 Launching..."
     #roslaunch実行
-    exec ros2 launch frootspi_examples robot.launch.py
+    exec ros2 launch frootspi_examples robot.launch.py id:=${ROBOT_ID}
 else
     echo "There is no ${ENVFILE}"
 fi
