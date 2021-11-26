e5c6bc98a86de316f6fa5c97b3e55629969a67fa
Makito Ishikura <tilt.silvie@gmail.com>
2021-11-27 01:00:20 +0900

    ロボット全台のFrootsPiを更新するスクリプトを実装

---
+++ frootspi_examples/utils/update.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# shutdown.sh
+# IPアドレスを指定して、SSHログインを行いシャットダウンします
+# 一気にシャットダウンしたいときは shutdown_all_robots.sh を使ってください
+#
+# Usage:
+# $ shutdown.sh IP_ADDRESS
+
+SSH_USER=ubuntu
+
+if [ -z "${1}" ]; then
+    echo "IPアドレスを渡してください"
+    exit 1
+fi
+
+IP_ADDR=${1}
+
+echo "=========="
+echo "PING test..."
+
+ping -c 1 -W 1 ${IP_ADDR}
+
+if [ $? -ne 0 ]; then
+    echo "PING Failed."
+    exit 2
+fi
+echo "PING OK!"
+
+echo "=========="
+echo "SSH Connect..."
+ssh ${SSH_USER}@${IP_ADDR} "sudo systemctl stop robot_launch"
+ssh ${SSH_USER}@${IP_ADDR} "cd ~/ros2_ws/src/FrootsPi && git switch main && git pull"
+ssh ${SSH_USER}@${IP_ADDR} "bash -c 'source /opt/ros/foxy/setup.bash && cd ~/ros2_ws/ && colcon build --symlink-install'"
+ssh ${SSH_USER}@${IP_ADDR} "sudo systemctl start robot_launch"
+
+echo "SSH Command completed"
+echo "=========="
+
