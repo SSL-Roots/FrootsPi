95dbfe442ba180cba6f3949b5faffe8fd015b2ea
Makito Ishikura <tilt.silvie@gmail.com>
2023-04-24 00:31:48 +0000

    未使用importを削除

--- frootspi_examples/launch/robot.launch.py
+++ frootspi_examples/launch/robot.launch.py
@@ -15,12 +15,10 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch.actions import ExecuteProcess
 from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessExit
 from launch.event_handlers import OnProcessStart
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.actions import PushRosNamespace
 from launch_ros.descriptions import ComposableNode
